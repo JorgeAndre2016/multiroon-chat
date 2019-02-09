f649274209474099b797b2ab7d7b5e147872fea7
Jorge <jorge_aquino@hotmail.com.br>
2019-02-09 10:09:10 -0200

    finalizado troca de mensagens e atualização de participantes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 var app = require('./config/server');
 
 /* parametrizar a porta de escuta */
-var server = app.listen(80, function(){
+var server = app.listen(80, function () {
     console.log('Servidor online');
 });
 
@@ -11,21 +11,36 @@ var io = require('socket.io').listen(server);
 app.set('io', io) // sentando uma variável do tipo socket dentro do express
 
 /* criar a conexão por websocket */
-io.on('connection', function(socket){ // evento disparado quando usuário entra do chat
+io.on('connection', function (socket) { // evento disparado quando usuário entra do chat
     console.log('Usuário conectou');
-    
-    socket.on('disconnect', function(){ // evento disparado quando usuário sai do chat
-        console.log('Usuário desconectou'); 
+
+    socket.on('disconnect', function () { // evento disparado quando usuário sai do chat
+        console.log('Usuário desconectou');
     });
 
-    socket.on('msgParaServidor', function(data){
+    socket.on('msgParaServidor', function (data) {
+
+        /* dialogo */
         socket.emit(
-            'mgsParaCliente', 
-            {apelido: data.apelido, mensagem: data.mensagem}
+            'mgsParaCliente',
+            { apelido: data.apelido, mensagem: data.mensagem }
         );
         socket.broadcast.emit(
-            'mgsParaCliente', 
-            {apelido: data.apelido, mensagem: data.mensagem}
+            'mgsParaCliente',
+            { apelido: data.apelido, mensagem: data.mensagem }
         );
+
+        /* participantes */
+
+        if (parseInt(data.apelido_atualizado_nos_clientes) == 0) {
+            socket.emit(
+                'participantesParaCliente',
+                { apelido: data.apelido }
+            );
+            socket.broadcast.emit(
+                'participantesParaCliente',
+                { apelido: data.apelido }
+            );
+        }
     });
 });
