739b32d1d0fe759077aa5f634b1b09857f8c25e1
Jorge <jorge_aquino@hotmail.com.br>
2019-02-09 09:44:43 -0200

    configurações do socket para enviar e receber mensagens cliente/servidor

--- app.js
+++ app.js
@@ -6,4 +6,26 @@ var server = app.listen(80, function(){
     console.log('Servidor online');
 });
 
-require('socket.io').listen(server);
+var io = require('socket.io').listen(server);
+
+app.set('io', io) // sentando uma variável do tipo socket dentro do express
+
+/* criar a conexão por websocket */
+io.on('connection', function(socket){ // evento disparado quando usuário entra do chat
+    console.log('Usuário conectou');
+    
+    socket.on('disconnect', function(){ // evento disparado quando usuário sai do chat
+        console.log('Usuário desconectou'); 
+    });
+
+    socket.on('msgParaServidor', function(data){
+        socket.emit(
+            'mgsParaCliente', 
+            {apelido: data.apelido, mensagem: data.mensagem}
+        );
+        socket.broadcast.emit(
+            'mgsParaCliente', 
+            {apelido: data.apelido, mensagem: data.mensagem}
+        );
+    });
+});
